ed3394abd853dc2d2d39d3e2b67c1d230bf4d1cf
JaakobLambot <jaakob.lambot@gmail.com>
2023-04-18 23:07:04 +0300

    seadmeSeaded.dart lisatud

--- lib/Login.dart
+++ lib/Login.dart
@@ -12,7 +12,7 @@ class LoginApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Demo',
+      title: 'Login',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
---
+++ lib/seadmeSeaded.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Device Settings',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DeviceSettingsPage(),
+    );
+  }
+}
+
+class DeviceSettingsPage extends StatefulWidget {
+  const DeviceSettingsPage({Key? key}) : super(key: key);
+
+  @override
+  _DeviceSettingsPageState createState() => _DeviceSettingsPageState();
+}
+
+class _DeviceSettingsPageState extends State<DeviceSettingsPage> {
+  late TextEditingController _controller;
+  late SharedPreferences _prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _loadPreferences();
+  }
+
+  Future<void> _loadPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+    String name = _prefs.getString('device_name') ?? 'My Device';
+    setState(() {
+      _controller.text = name;
+    });
+  }
+
+  Future<void> _savePreferences(String name) async {
+    await _prefs.setString('device_name', name);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Device Settings'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Device Name',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter device name',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () async {
+                String name = _controller.text.trim();
+                await _savePreferences(name);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Device name saved: $name'),
+                  ),
+                );
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
