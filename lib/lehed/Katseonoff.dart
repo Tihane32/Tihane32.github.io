7cbea58fae8fc5ba1fcae7520dfac6168a1f4ad5
JaakobLambot <jaakob.lambot@gmail.com>
2023-05-11 23:16:55 +0300

    visuaalne lulitusgraafik

--- lib/lehed/Katseonoff.dart
+++ lib/lehed/Katseonoff.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:testuus4/funktsioonid/Elering.dart';
 
 void main() {
   runApp(GraafikAPP());
@@ -25,35 +28,140 @@ class _LulitusGraafik extends StatefulWidget {
 class _LulitusGraafikState extends State<_LulitusGraafik> {
   late Map<String, dynamic> lulitus;
 
+  Future test() async {
+    lulitus = {
+      '0000': ['00.00', 0, false],
+      '0100': ['01.00', 0, false],
+      '0200': ['02.00', 0, true],
+      '0300': ['03.00', 0, false],
+      '0400': ['04.00', 0, true],
+      '0500': ['05.00', 0, true],
+      '0600': ['06.00', 0, false],
+      '0700': ['07.00', 0, true],
+      '0800': ['08.00', 0, false],
+      '0900': ['09.00', 0, true],
+      '1000': ['10.00', 0, false],
+      '1100': ['11.00', 0, true],
+      '1200': ['12.00', 0, true],
+      '1300': ['13.00', 0, true],
+      '1400': ['14.00', 0, true],
+      '1500': ['15.00', 0, true],
+      '1600': ['16.00', 0, true],
+      '1700': ['17.00', 0, true],
+      '1800': ['18.00', 0, false],
+      '1900': ['19.00', 0, true],
+      '2000': ['20.00', 0, false],
+      '2100': ['21.00', 0, true],
+      '2200': ['22.00', 0, false],
+      '2300': ['23.00', 0, false],
+    };
+    DateTime now = DateTime.now();
+    int tana = now.weekday - 1;
+
+    List<String> newList = [];
+    var headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
+    var data = {
+      'channel': '0',
+      'id': '80646f81ad9a',
+      'auth_key':
+          'MTcxYTNjdWlk99F3FBAA1664CCBA1DB1A6FE1A156072DB6AF5A8F1B4A1B0ACB7C82002649DADF1114D97483D4E12',
+    };
+
+    var url = Uri.parse('https://shelly-64-eu.shelly.cloud/device/settings');
+    var res = await http.post(url, headers: headers, body: data);
+    await Future.delayed(const Duration(seconds: 2));
+    //Kui post läheb läbi siis:
+
+    final httpPackageJson = json.decode(res.body) as Map<String, dynamic>;
+
+    final scheduleRules1 = httpPackageJson['data']['device_settings']['relays']
+        [0]['schedule_rules'];
+
+    for (String item in scheduleRules1) {
+      List<String> parts = item.split('-');
+      if (parts[1].length > 1) {
+        for (int i = 0; i < parts[1].length; i++) {
+          //lülituskäsk tehakse iga "-" juures pooleks ja lisatakse eraldi listi
+          String newItem = '${parts[0]}-${parts[1][i]}-${parts[2]}';
+          newList.add(newItem);
+        }
+      } else {
+        newList.add(item);
+      }
+    }
+    List<String> filteredRules = [];
+
+    RegExp regExp = RegExp("-$tana-");
+
+    for (var rule in newList) {
+      if (regExp.hasMatch(rule)) {
+        filteredRules.add(rule);
+      }
+    }
+    var elering = await getElering('täna');
+    var i = 0;
+
+    setState(() {
+      print(elering[0]['price']);
+
+      i = filteredRules.length;
+      var u = 0;
+
+      bool k = false;
+      for (var j = 0; j < 24; j++) {
+        String asendus = '$j';
+        if (j < 10) {
+          asendus = '0' + asendus + '00';
+        } else {
+          asendus = asendus + '00';
+        }
+        for (u = 0; u < i; u++) {
+          List<String> parts = filteredRules[u].split('-');
+
+          String timeString = parts[0];
+          String formattedTime =
+              timeString.substring(0, 2) + '.' + timeString.substring(2);
+
+          if (formattedTime == lulitus[asendus][0]) {
+            print('oige: $j');
+            if (parts[2] == 'on') {
+              lulitus[asendus][2] = true;
+              k = true;
+              print('sisse');
+            } else {
+              lulitus[asendus][2] = false;
+              k = false;
+              print('välja');
+            }
+          } /*else {
+            if (j != 0) {
+              var ajutine = j - 1;
+
+              String asendus1 = '$ajutine';
+              if (ajutine < 10) {
+                asendus1 = '0' + asendus1 + '00';
+              } else {
+                asendus1 = asendus1 + '00';
+              }
+
+              lulitus[asendus][2] = lulitus[asendus1][2];
+            }
+          }*/
+          //print(lulitus['$j']);
+        }
+        lulitus[asendus][2] = k;
+        lulitus[asendus][1] = elering[j]['price'];
+      }
+    });
+  }
+
   @override
   void initState() {
+    test();
     super.initState();
-    lulitus = {
-      '00': ['00.00', 12, false],
-      '01': ['01.00', 15, false],
-      '02': ['02.00', 30, true],
-      '03': ['03.00', 6.4, false],
-      '04': ['04.00', 14, true],
-      '05': ['05.00', 18, true],
-      '06': ['06.00', 20, false],
-      '07': ['07.00', 12, true],
-      '08': ['08.00', 8, false],
-      '09': ['09.00', 22, true],
-      '10': ['10.00', 16, false],
-      '11': ['11.00', 19, true],
-      '12': ['12.00', 24, true],
-      '13': ['13.00', 28, true],
-      '14': ['14.00', 60, true],
-      '15': ['15.00', 32, true],
-      '16': ['16.00', 36, true],
-      '17': ['17.00', 34, true],
-      '18': ['18.00', 28, false],
-      '19': ['19.00', 24, true],
-      '20': ['20.00', 20, false],
-      '21': ['21.00', 18, true],
-      '22': ['22.00', 14, true],
-      '23': ['23.00', 12, false],
-    };
   }
 
   @override
