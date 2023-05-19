e98c0eefb5664b14f3707de92e28581191896806
Trevor <truuna@taltech.ee>
2023-05-19 09:56:25 +0300

    gi

--- lib/lehed/hindJoonisel.dart
+++ lib/lehed/hindJoonisel.dart
@@ -50,18 +50,18 @@ class _TulpDiagrammState extends State<TulpDiagramm> {
   late double hindAVG;
   Map<int, dynamic> keskHind = {
     0: ['0', 0],
-    1: ['0', 0],
-    2: ['0', 0],
-    3: ['0', 0],
-    4: ['0', 0],
-    5: ['0', 0],
-    6: ['0', 0],
-    7: ['0', 0],
-    8: ['0', 0],
-    9: ['0', 0],
-    10: ['0', 0],
-    11: ['0', 0],
-    12: ['0', 0],
+    1: ['0.0', 0],
+    2: ['1', 0],
+    3: ['2', 0],
+    4: ['3', 0],
+    5: ['4', 0],
+    6: ['5', 0],
+    7: ['6', 0],
+    8: ['7', 0],
+    9: ['8', 0],
+    10: ['9', 0],
+    11: ['10', 0],
+    12: ['12', 0],
     13: ['0', 0],
     14: ['0', 0],
     15: ['0', 0],
@@ -91,12 +91,12 @@ class _TulpDiagrammState extends State<TulpDiagramm> {
         10: ['10.00', 4, false],
         11: ['11.00', 4, true],
         12: ['12.00', 4, true],
-        13: ['13.00', 22, true],
-        14: ['14.00', 40, true],
-        15: ['15.00', 30, true],
-        16: ['16.00', 10, true],
-        17: ['17.00', 11, true],
-        18: ['18.00', 22, false],
+        13: ['13.00', 22.55, true],
+        14: ['14.00', 40.567, true],
+        15: ['15.00', 44.4, true],
+        16: ['16.00', 80, true],
+        17: ['17.00', 121, true],
+        18: ['18.00', 100.2, false],
         19: ['19.00', 0, true],
         20: ['20.00', 22, false],
         21: ['21.00', 13.5, true],
@@ -140,32 +140,44 @@ class _TulpDiagrammState extends State<TulpDiagramm> {
           ],
         ),
 
-        body: Container(
-          child: SfCartesianChart(
-            primaryXAxis: CategoryAxis(),
-            primaryYAxis: NumericAxis(),
-            series: [
-              BarSeries(
-                dataSource: lulitus.values.toList(),
-                xValueMapper: (inf, _) => inf[0],
-                yValueMapper: (inf, _) => inf[1],
-                dataLabelMapper: (data, _) => data[1].toString() + '€/MWh',
-                pointColorMapper: (data, _) => Colors.green,
-                dataLabelSettings: DataLabelSettings(
-                  isVisible: true,
-                  labelAlignment: ChartDataLabelAlignment.middle,
-                  textStyle: TextStyle(fontSize: 10, color: Colors.black),
+        body: Center(
+            child: RotatedBox(
+          quarterTurns: 1,
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(
+                  labelRotation: 270, visibleMinimum: 0, visibleMaximum: 23),
+              primaryYAxis: NumericAxis(
+                labelRotation: 270,
+                minimum: 0,
+              ),
+              series: <ChartSeries>[
+                ColumnSeries(
+                  dataSource: lulitus.values.toList(),
+                  xValueMapper: (data, _) => data[0],
+                  yValueMapper: (data, _) => data[1],
+                  dataLabelMapper: (data, _) => data[1].toString() + '€/MWh',
+                  pointColorMapper: (data, _) => Colors.green,
+                  dataLabelSettings: DataLabelSettings(
+                    isVisible: true,
+                    labelAlignment: ChartDataLabelAlignment.middle,
+                    textStyle: TextStyle(fontSize: 15, color: Colors.black),
+                    angle: 270,
+                  ),
                 ),
-              ),
-              LineSeries(
-                dataSource: keskHind.values.toList(),
-                xValueMapper: (inf, _) => inf[0],
-                yValueMapper: (inf, _) => inf[1],
-                color: Color.fromARGB(255, 197, 44, 33),
-              ),
-            ],
+                LineSeries(
+                  dataSource: keskHind.values.toList(),
+                  xValueMapper: (inf, _) => inf[0],
+                  yValueMapper: (inf, _) => inf[1],
+                  width: 1,
+                  color: Color.fromARGB(255, 197, 44, 33),
+                ),
+              ],
+            ),
           ),
-        ),
+        )),
 
         bottomNavigationBar: BottomNavigationBar(
             backgroundColor: Color.fromARGB(255, 115, 162, 195),
@@ -243,6 +255,7 @@ keskmineHindMapVaartustamine(var hindAVG, Map<int, dynamic> keskHind) {
     }
     keskHind[i] = [kell + '.00', hindAVG];
   }
+
   keskHind.forEach((key, value) {
     print('$key: $value');
   });
