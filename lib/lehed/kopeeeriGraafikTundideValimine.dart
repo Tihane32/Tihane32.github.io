aa9729942117e2d9e29995920d3d370a98121750
Trevor <truuna@taltech.ee>
2023-09-04 11:25:24 +0300

    graafiku kopeerimine lisatud

--- lib/lehed/kopeeeriGraafikTundideValimine.dart
+++ lib/lehed/kopeeeriGraafikTundideValimine.dart
@@ -1,16 +1,14 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:testuus4/lehed/GraafikusseSeadmeteValik.dart';
 import 'package:testuus4/lehed/dynamicKoduLeht.dart';
 import 'package:testuus4/lehed/keskimiseHinnaAluselTundideValimine.dart';
+import '../funktsioonid/seisukord.dart';
 import '../main.dart';
 import 'AbiLeht.dart';
-import 'koduleht.dart';
 import 'hinnaPiiriAluselTunideValimine.dart';
-import 'dart:math';
-import 'package:testuus4/lehed/kaksTabelit.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 
 class KopeeriGraafikTundideValik extends StatefulWidget {
   const KopeeriGraafikTundideValik({Key? key}) : super(key: key);
@@ -20,75 +18,74 @@ class KopeeriGraafikTundideValik extends StatefulWidget {
       _KopeeriGraafikTundideValikState();
 }
 
-int koduindex = 1;
-Color valge = Colors.white;
-Color green = Colors.green;
-Color homme = valge;
-Color tana = green;
-TextStyle hommeFont = font;
-TextStyle tanaFont = fontValge;
-int? tappedIndex;
-
 class _KopeeriGraafikTundideValikState
     extends State<KopeeriGraafikTundideValik> {
-  late Map<int, dynamic> lulitusMap;
-  int selectedRowIndex = -1;
-  late double hindAVG;
-  String paevNupp = 'Täna';
+  int koduindex = 1;
+  bool isLoading = true;
   String selectedPage = 'Kopeeri graafik';
   double vahe = 10;
   int valitudTunnid = 10;
   Color boxColor = sinineKast;
-
   Map<String, bool> ValitudGraafik = {};
-  Map<String, List<String>> SeadmeteMap = {
-    'Keldri boiler': [
-      'assets/boiler1.jpg',
-      '123456',
-      'off',
-      'Shelly plug S',
-      'jah',
-    ],
-    'Veranda lamp': [
-      'assets/verandaLamp1.png',
-      '123456',
-      'offline',
-      'Shelly plug S',
-      'ei',
-    ],
-    'Keldri pump': [
-      'assets/pump1.jpg',
-      '123456',
-      'on',
-      'Shelly plug S',
-      'jah',
-    ],
-    'Garaazi pump': [
-      'assets/pump1.jpg',
-      '123456',
-      'offline',
-      'Shelly plug S',
-      'jah',
-    ],
-    'Main boiler': [
-      'assets/boiler1.jpg',
-      '123456',
-      'on',
-      'Shelly plug S',
-      'jah',
-    ],
-    'Sauna boiler': [
-      'assets/boiler1.jpg',
-      '123456',
-      'off',
-      'Shelly plug S',
-      'ei',
-    ],
-  };
 
+  late Map<String, List<String>> minuSeadmedK = {};
+  dynamic seadmeGraafik;
+  @override
   void initState() {
+    //seisukord();
+    _submitForm();
     super.initState();
     ValitudGraafik = valitudSeadmeteNullimine(SeadmeteMap);
+  }
+
+  Map<String, List<String>> SeadmeteMap = {};
+  Set<String> selectedPictures = Set<String>();
+
+  void toggleSelection(String pictureName) {
+    setState(() {
+      if (selectedPictures.contains(pictureName)) {
+        selectedPictures.remove(pictureName);
+      } else {
+        selectedPictures.add(pictureName);
+      }
+    });
+  }
+
+  Future _submitForm() async {
+    minuSeadmedK.clear();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //await prefs.clear();
+
+    String? storedJsonMap = prefs.getString('seadmed');
+    print(storedJsonMap);
+    if (storedJsonMap != null) {
+      await seisukord();
+      storedJsonMap = prefs.getString('seadmed');
+      Map<String, dynamic> storedMap = json.decode(storedJsonMap!);
+      await Future.delayed(const Duration(seconds: 3));
+      var i = 0;
+      for (String Seade in storedMap.keys) {
+        var id = storedMap['Seade$i']['Seadme_ID'];
+        var name = storedMap['Seade$i']['Seadme_nimi'];
+        var pistik = storedMap['Seade$i']['Seadme_pistik'];
+        var olek = storedMap['Seade$i']['Seadme_olek'];
+        print('olek: $olek');
+        Map<String, List<String>> ajutineMap = {
+          name: ['assets/boiler1.jpg', '$id', '$olek', '$pistik', 'jah'],
+        };
+        minuSeadmedK.addAll(ajutineMap);
+        i++;
+      }
+      //TODO: eemalda j'rgmine rida kui gen2 tgraafik tootab
+      minuSeadmedK['Shelly Pro PM']![4] = 'ei';
+      print('seadmed');
+      print(minuSeadmedK);
+      print(SeadmeteMap);
+    }
+    setState(() {
+      SeadmeteMap = minuSeadmedK;
+      isLoading = false;
+    });
   }
 
   @override
@@ -158,139 +155,146 @@ class _KopeeriGraafikTundideValikState
             ),
           ],
         ),
-        body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 1,
-            childAspectRatio: MediaQuery.of(context).size.width /
-                (MediaQuery.of(context).size.height / 5),
-          ),
-          itemCount: SeadmeteMap.length,
-          itemBuilder: (context, index) {
-            final seade = SeadmeteMap.keys.elementAt(index);
-            final pilt = SaaSeadmePilt(SeadmeteMap, seade);
-            final staatus = SaaSeadmeolek(SeadmeteMap, seade);
-            final graafik = SaaSeadmegraafik(SeadmeteMap, seade);
-            return GestureDetector(
-              onTap: () {
-                setState(() {
-                  ValitudGraafik = valitudSeadmeteNullimine(SeadmeteMap);
-                });
-                if (graafik == 'jah') {
-                  setState(() {
-                    if (ValitudGraafik[seade] == false) {
-                      ValitudGraafik[seade] = true;
-                    }
-                  });
-                } else {
-                  showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                            title: Text("  Seadmel puudub graafik"),
-                          ));
-                }
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(1),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: border,
+        body: GestureDetector(
+          child: isLoading
+              ? Center(child: CircularProgressIndicator())
+              : GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    childAspectRatio: MediaQuery.of(context).size.width /
+                        (MediaQuery.of(context).size.height / 5),
                   ),
-                  child: Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: ValitudGraafik[seade] == true
-                            ? Colors.green
-                            : Colors.grey,
-                        width: 8,
-                      ),
-                    ),
-                    child: Stack(
-                      children: [
-                        Container(
-                            color: ValitudGraafik[seade] == true
-                                ? Color.fromARGB(255, 177, 245, 180)
-                                : Color.fromARGB(255, 236, 228, 228)),
-                        AspectRatio(
-                          aspectRatio: 1,
-                          child: ClipRRect(
-                            child: Image.asset(
-                              pilt,
-                              fit: BoxFit.cover,
-                            ),
+                  itemCount: SeadmeteMap.length,
+                  itemBuilder: (context, index) {
+                    final seade = SeadmeteMap.keys.elementAt(index);
+                    final pilt = SaaSeadmePilt(SeadmeteMap, seade);
+                    final staatus = SaaSeadmeolek(SeadmeteMap, seade);
+                    final graafik = SaaSeadmegraafik(SeadmeteMap, seade);
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          ValitudGraafik =
+                              valitudSeadmeteNullimine(SeadmeteMap);
+                        });
+                        if (graafik == 'jah') {
+                          setState(() {
+                            if (ValitudGraafik[seade] == false) {
+                              ValitudGraafik[seade] = true;
+                            }
+                          });
+                        } else {
+                          showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                    title: Text("  Seadmel puudub graafik"),
+                                  ));
+                        }
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: border,
                           ),
-                        ),
-                        Positioned(
-                          top: 8,
-                          right: 8,
-                          child: graafik == 'ei'
-                              ? IconButton(
-                                  iconSize: 60,
-                                  icon: Icon(
-                                    Icons.warning_amber_rounded,
-                                    size: 80,
-                                    color: Colors.amber,
-                                  ),
-                                  onPressed: () {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) => AlertDialog(
-                                              title: Text(
-                                                  "  $seade graafik puudub"),
-                                            ));
-                                  },
-                                )
-                              : IconButton(
-                                  iconSize: 60,
-                                  icon: Icon(
-                                    Icons.fact_check_outlined,
-                                    size: 80,
-                                    color: Colors.blue,
-                                  ),
-                                  onPressed: () {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) => AlertDialog(
-                                              title: Text(
-                                                  "  $seade graafik: \n \t 00.00 on \n \t 00.10 on \n \t 00.20 on \n \t 03.00 off \n \t ..."),
-                                            ));
-                                  },
-                                ),
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          left: 0,
-                          right: 0,
                           child: Container(
-                            color: Colors.blue.withOpacity(0.6),
-                            padding: EdgeInsets.symmetric(vertical: 8),
-                            child: Center(
-                              child: Text(
-                                seade,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            width: double.infinity,
+                            height: double.infinity,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: ValitudGraafik[seade] == true
+                                    ? Colors.green
+                                    : Colors.grey,
+                                width: 8,
                               ),
                             ),
+                            child: Stack(
+                              children: [
+                                Container(
+                                    color: ValitudGraafik[seade] == true
+                                        ? Color.fromARGB(255, 177, 245, 180)
+                                        : Color.fromARGB(255, 236, 228, 228)),
+                                AspectRatio(
+                                  aspectRatio: 1,
+                                  child: ClipRRect(
+                                    child: Image.asset(
+                                      pilt,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 8,
+                                  right: 8,
+                                  child: graafik == 'ei'
+                                      ? IconButton(
+                                          iconSize: 60,
+                                          icon: Icon(
+                                            Icons.warning_amber_rounded,
+                                            size: 80,
+                                            color: Colors.amber,
+                                          ),
+                                          onPressed: () {
+                                            showDialog(
+                                                context: context,
+                                                builder: (context) =>
+                                                    AlertDialog(
+                                                      title: Text(
+                                                          "  $seade graafik puudub"),
+                                                    ));
+                                          },
+                                        )
+                                      : IconButton(
+                                          iconSize: 60,
+                                          icon: Icon(
+                                            Icons.fact_check_outlined,
+                                            size: 80,
+                                            color: Colors.blue,
+                                          ),
+                                          onPressed: () {
+                                            showDialog(
+                                                context: context,
+                                                builder: (context) =>
+                                                    AlertDialog(
+                                                      title: Text(
+                                                          "  $seade graafik: \n \t 00.00 on \n \t 00.10 on \n \t 00.20 on \n \t 03.00 off \n \t ..."),
+                                                    ));
+                                          },
+                                        ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  left: 0,
+                                  right: 0,
+                                  child: Container(
+                                    color: Colors.blue.withOpacity(0.6),
+                                    padding: EdgeInsets.symmetric(vertical: 8),
+                                    child: Center(
+                                      child: Text(
+                                        seade,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ],
-                    ),
-                  ),
+                      ),
+                    );
+                  },
                 ),
-              ),
-            );
-          },
         ),
         bottomNavigationBar: BottomNavigationBar(
             backgroundColor: Color.fromARGB(255, 115, 162, 195),
             fixedColor: Color.fromARGB(255, 157, 214, 171),
             unselectedItemColor: Colors.white,
-            selectedIconTheme: IconThemeData(size: 40),
-            unselectedIconTheme: IconThemeData(size: 30),
+            //selectedIconTheme: IconThemeData(size: 40),
+            //unselectedIconTheme: IconThemeData(size: 30),
             items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(
                 label: 'Seadmed',
