2d7480594b486a3fab03320f614613291ad09831
jalamb <jalamb@taltech.ee>
2023-09-26 17:08:36 +0300

    uuenfused

---
+++ lib/funktsioonid/tarbmineSeadmeKohta.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:testuus4/funktsioonid/token.dart';
+import 'package:testuus4/main.dart';
+import 'package:http/http.dart' as http;
+
+getTarbimine(String seadmeID, String algusPaev, String vahemik) async {
+  String token = await getToken2();
+
+  var headers = {
+    'Authorization': 'Bearer $token',
+  };
+  print(algusPaev);
+  DateFormat format = DateFormat("yyyy-MM-dd HH:mm:ss");
+  DateTime dateTime = format.parse(algusPaev);
+  DateTime newDateTime =
+      DateTime(dateTime.year, dateTime.month, dateTime.day, 23, 59, 59);
+  print(dateTime);
+  print(newDateTime);
+  var data = {
+    'id': seadmeID,
+    'channel': '0',
+    'date_range': 'custom',
+    'date_from': '$dateTime',
+    'date_to': '$newDateTime',
+  };
+
+  var url = Uri.parse(
+      'https://shelly-64-eu.shelly.cloud/statistics/relay/consumption');
+  var res = await http.post(url, headers: headers, body: data);
+  print(res.body);
+  final jsonData = json.decode(res.body);
+  final historyData = jsonData['data']['history'] as List<dynamic>;
+  print(historyData);
+  List<double> abi = [];
+  for (var i = 0; i < historyData.length;) {
+    print(historyData[i]['consumption']);
+
+    if (historyData[i]['consumption'] == 0) {
+      abi.add(0.0);
+    }else{
+      abi.add(historyData[i]['consumption']);
+    }
+    
+    i++;
+  }
+
+  return abi;
+}
--- lib/lehed/paevaGraafik.dart
+++ lib/lehed/paevaGraafik.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:testuus4/funktsioonid/tarbmineSeadmeKohta.dart';
 import 'package:testuus4/main.dart';
 
-double findMaximumYValue(List<ChartDataPoint> dataPoints) {
+double findMaximumYValue(dataPoints) {
   double max = double.negativeInfinity;
   for (int i = 0; i < dataPoints.length; i++) {
     if (dataPoints[i].y > max) {
@@ -12,7 +13,7 @@ double findMaximumYValue(List<ChartDataPoint> dataPoints) {
   return max;
 }
 
-class PaevaTarbimine extends StatelessWidget {
+class PaevaTarbimine extends StatefulWidget {
   final String date;
   final String value;
   final int rowIndex;
@@ -26,15 +27,44 @@ class PaevaTarbimine extends StatelessWidget {
   });
 
   @override
+  State<PaevaTarbimine> createState() => _PaevaTarbimineState();
+}
+
+class _PaevaTarbimineState extends State<PaevaTarbimine> {
+  List<double> paevaTarbimine = [];
+
+  getList() async {
+    var temp = await getTarbimine(widget.value, widget.date, "day");
+    setState(() {
+      paevaTarbimine = temp;
+      print(paevaTarbimine);
+    });
+  }
+
+  getElering() {}
+
+  @override
+  void initState() {
+    getList();
+    getElering();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final List<double> temp = paevaMaksumus[rowIndex]!;
+    final List<double> temp = widget.paevaMaksumus[widget.rowIndex]!;
     final List<int> xValues = List.generate(temp.length, (index) => index);
 
     final List<ChartDataPoint> dataPoints = [];
     for (int i = 0; i < temp.length; i++) {
       dataPoints.add(ChartDataPoint(x: xValues[i], y: temp[i]));
     }
+    final List<ChartDataPoint> dataPoints2 = [];
+    for (int i = 0; i < paevaTarbimine.length; i++) {
+      dataPoints2.add(ChartDataPoint(x: xValues[i], y: paevaTarbimine[i]));
+    }
     double maxYValue = findMaximumYValue(dataPoints);
+    double maxYValue2 = findMaximumYValue(dataPoints2);
     return Container(
       height: MediaQuery.of(context).size.height * 1.5,
       child: RotatedBox(
@@ -77,21 +107,41 @@ class PaevaTarbimine extends StatelessWidget {
           ),
           series: <ChartSeries>[
             StackedColumnSeries<ChartDataPoint, int>(
+              color: Colors.blue,
               width: 0.9,
               borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(20), topRight: Radius.circular(20)),
               //splineType: SplineType.monotonic,
-              dataSource: dataPoints,
+              dataSource: dataPoints2,
               yAxisName: 'firstAxis',
               dataLabelSettings: DataLabelSettings(
-                offset: Offset(0, -10),
+                offset: Offset(0, 0),
                 isVisible: true,
-                labelAlignment: ChartDataLabelAlignment.outer,
+                labelAlignment: ChartDataLabelAlignment.bottom,
                 textStyle: fontVaike,
                 angle: 270,
               ),
               xValueMapper: (ChartDataPoint data, _) => data.x,
-              yValueMapper: (ChartDataPoint data, _) => data.y,
+              yValueMapper: (ChartDataPoint data, _) {
+                final yValue = data.y;
+                return yValue == 0
+                    ? 0
+                    : yValue < maxYValue2 * 0.4
+                        ? maxYValue2 * 0.4
+                        : yValue;
+              },
+
+              dataLabelMapper: (data, _) {
+                // Display the data label only if the consumption is not 0
+                if (data.y == 0) {
+                  return ''; // Customize this as needed
+                } else {
+                  String temp3 = "";
+
+                  temp3 = data.y.toStringAsFixed(2);
+                  return '$temp3 Wh';
+                }
+              },
             ),
             StackedColumnSeries<ChartDataPoint, int>(
                 //splineType: SplineType.monotonic,
@@ -104,9 +154,9 @@ class PaevaTarbimine extends StatelessWidget {
                 yAxisName: 'secondAxis',
                 groupName: 'A',
                 dataLabelSettings: DataLabelSettings(
-                  offset: Offset(0, -20),
+                  offset: Offset(0, 0),
                   isVisible: true,
-                  labelAlignment: ChartDataLabelAlignment.outer,
+                  labelAlignment: ChartDataLabelAlignment.bottom,
                   textStyle: fontVaike,
                   angle: 270,
                 ),
