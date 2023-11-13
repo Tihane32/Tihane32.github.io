e02ae370afe88737d8b657b2898c2e401f0a9c08
jalamb <jalamb@taltech.ee>
2023-11-13 12:37:58 +0200

    unused imports

--- lib/lehed/Põhi_Lehed/Login.dart
+++ lib/lehed/Põhi_Lehed/Login.dart
@@ -4,16 +4,9 @@ import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:testuus4/lehed/P%C3%B5hi_Lehed/dynamicKoduLeht.dart';
-import 'package:testuus4/Arhiiv/kaksTabelit.dart';
-import 'package:testuus4/lehed/lisaSeade.dart';
 //import '/SeadmeSeaded.dart';
-import 'package:testuus4/lehed/Seadme_Lehed/seadmeSeaded.dart';
 import 'package:testuus4/lehed/uuedSeadmed.dart';
 import 'package:testuus4/main.dart';
-import '../../Arhiiv/energiaGraafik.dart';
-import 'package:testuus4/funktsioonid/seisukord.dart';
-import 'package:testuus4/lehed/P%C3%B5hi_Lehed/koduleht.dart';
-import 'package:google_fonts/google_fonts.dart';
 import '../uuedSeadmed.dart';
 
 class LoginPage extends StatefulWidget {
